fe39b8f65ae8c9f81a87fa1cf80797b77f0f6e04
rainaldo <rainaldo@gutsgusto.com>
2025-02-09 13:36:30 +0100

    MESSAGE

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
 use App\Models\Message;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Inertia\Inertia;
 
 class ChatController extends Controller
 {
-    public function index(): View
+    public function index()
     {
-        return view('chat');
+        return Inertia::render('chat');
     }
 
     public function messages(): Collection
